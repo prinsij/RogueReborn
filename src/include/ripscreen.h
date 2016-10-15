a657e592ddab6e14145d6ede6628afaf442eba75
prinsij <prinsij@mcmaster.ca>
2016-10-15 00:30:22 -0500

    Begin tracking/storing/displaying scores

--- src/include/ripscreen.h
+++ src/include/ripscreen.h
@@ -2,11 +2,19 @@
 #include "uistate.h"
 #include "playerchar.h"
 #include "../libtcod/include/libtcod.hpp"
+#include <vector>
+#include <string>
+#include <sstream>
+
+struct ScoreItem;
 
 class RIPScreen : public UIState {
 	public:
 		RIPScreen(PlayerChar*);
 		virtual void draw(TCODConsole*);
+		virtual UIState* handleInput(TCOD_key_t);
 	private:
 		PlayerChar* player;
+		std::vector<ScoreItem> scores;
+		const std::string SCORE_FILE = "scores.txt";
 };
