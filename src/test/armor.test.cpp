d7855adc3fd9ad93f975ac2a4d7d4d3bc788d0ff
Or Almog <almogo@mcmaster.ca>
2016-10-23 19:42:27 -0400

    modified makefiles

--- src/test/armor.test.cpp
+++ src/test/armor.test.cpp
@@ -3,10 +3,22 @@
 #include <iostream>
 
 #include "../include/armor.h"
+#include "../armor.cpp"
 
+#include "../include/coord.h"
+#include "../coord.cpp"
+
+#include "../include/item.h"
+#include "../item.cpp"
+
+#include "../include/feature.h"
+#include "../feature.cpp"
 
 int main(){
 
-	std::cout << "Hello" << std::endl;
+	Armor a = Armor(Coord(5,5));
 
+	std::cout << a.getRating() << std::endl;
+
+	return 0;
 }
