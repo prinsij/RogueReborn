e90a1af304cb376168ec01df0a88e0a4d74f03be
Or Almog <almogo@mcmaster.ca>
2016-11-02 09:50:11 -0400

    sample test

--- Pipeline/check.py
+++ Pipeline/check.py
@@ -5,12 +5,17 @@ results = file("TestResults", "r").readlines()
 
 x = False
 
+#Hashtags can be used for comments in the test files
+#Otherwise, only a first character of "0" means success
+#Anything else will fail!
 for i in results:
-	if "0" in i:
+	print "[PYTHON TESTER] " + i
+	if "0" != i[0] and "#" != i[0]:
 		x = True
+		break
 
 if (x):
 	print "Failed!"
 	os.system("make -C ./.fail/")
 else:
-	print "OK!"
+	print "[PYTHON TESTER] All tests have passed successfully!"
--- src/test/test.armor.cpp
+++ src/test/test.armor.cpp
@@ -12,11 +12,16 @@
 class ArmorTest : public Testable {
 	public:
 		ArmorTest(){
-			std::cout << "Creating armor test" << std::endl;
+			std::cout << "# Creating armor test" << std::endl;
 		}
 
 		bool test(){
-			std::cout << "Testing armor!" << std::endl;
+			std::cout << "# Testing armor!" << std::endl;
+
+			std::cout << "0 Test 1 success" << std::endl;
+			std::cout << "0 Test 2 success" << std::endl;
+			std::cout << "0 Test 3 success" << std::endl;
+
 			return true;
 		}
 };
