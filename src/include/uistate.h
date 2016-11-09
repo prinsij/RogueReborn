65743ecd59bc5e7f8ab9088ce659afe6b4eab621
prinsij <prinsij@mcmaster.ca>
2016-11-09 08:59:06 -0600

    comment uistate.h

--- src/include/uistate.h
+++ src/include/uistate.h
@@ -9,13 +9,19 @@
 
 #include "../libtcod/include/libtcod.hpp"
 
+/** Class modeling a state of the game interface.
+ * Game transitions between these states like a
+ * finite state machine.
+ */
 class UIState {
 	public:
 		UIState();
-		// Render the current UI
+		/** Render the current UI */
 		virtual void draw(TCODConsole*);
-		// Do whatever is needed in response to keypresses
-		// then return state to transition to (can be self)
+		/** Do whatever is needed in response to keypresses
+		 *  then return state to transition to (can be self).
+		 */
 		virtual UIState* handleInput(TCOD_key_t);
+		/** Destructor */
 		virtual ~UIState();
 };
