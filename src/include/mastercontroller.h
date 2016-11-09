0ec5e1dc2de2ea8d0ebce89876dded037642c908
prinsij <prinsij@mcmaster.ca>
2016-11-09 09:02:06 -0600

    comment mastercontroller.h

--- src/include/mastercontroller.h
+++ src/include/mastercontroller.h
@@ -10,11 +10,21 @@
 #include "../libtcod/include/libtcod.hpp"
 #include "uistate.h"
 
+/** Controls the top level flow flow of the
+ *  application and main game loop.
+ *  Called directly from main.
+ */
 class MasterController {
 	public:
+		/** All game logic is inside, so no params
+		 *  needed for constructor
+		 */
 		MasterController();
+		/** Main game loop */
 		void run();
 	private:
+		/** Current interface state */
 		UIState* currState;
+		/** Max number of frames per second */
 		const int FPS_LIMIT = 20;
 };
