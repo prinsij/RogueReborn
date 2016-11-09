c2219485e2b93d95faa1d4bc0abf0f5fdb5e2325
prinsij <prinsij@mcmaster.ca>
2016-11-09 10:13:24 -0600

    comment tiles.h

--- src/include/tiles.h
+++ src/include/tiles.h
@@ -9,21 +9,34 @@
 
 #include "terrain.h"
 
+/** Regular dungeon floor. 
+ * Has full visibility and passability.
+ */
 class Floor : public Terrain {
 	public:
 		Floor();
 };
 
+/** Regular dungeon wall.
+ * Has no visiblity or passability.
+ */
 class Wall : public Terrain {
 	public:
 		Wall();
 };
 
+/** Regular corridor tile.
+ * Has limited visibility and full passability
+ */
 class Corridor : public Terrain {
 	public:
 		Corridor();
 };
 
+/** Door tile.
+ * Only cosmetically different from corridor tile.
+ * @see Corridor
+ */
 class Door : public Terrain {
 	public:
 		Door();
