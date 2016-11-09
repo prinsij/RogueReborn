d70b9fa8f35eb01596cf5f595c4592e6b7c4ab56
prinsij <prinsij@mcmaster.ca>
2016-11-09 15:48:54 -0600

    comment goldpile.h

--- src/include/goldpile.h
+++ src/include/goldpile.h
@@ -10,10 +10,21 @@
 #include "coord.h"
 #include "feature.h"
 
+/** Represents a pile of gold on the ground,
+ *  which can be picked up by the player to
+ *  enhance their score.
+ */
 class GoldPile : public Feature {
 	public:
+		/** Constructor of location, quantity. */
 		GoldPile(Coord, int);
+		/** Getter for quantity.
+		 * @see quantity
+		 */
 		int getQuantity();
 	private:
+		/** Amount of gold.
+		 * Should be positive, non-zero.
+		 */
 		int quantity;
 };
