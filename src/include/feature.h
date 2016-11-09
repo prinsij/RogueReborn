33a5e3b03c9742c216e5c0761e9c22a5aeafcf7e
prinsij <prinsij@mcmaster.ca>
2016-11-09 15:38:43 -0600

    comment feature.h

--- src/include/feature.h
+++ src/include/feature.h
@@ -9,14 +9,36 @@
 
 #include "coord.h"
 
+/** Models a 'thing' in the dungeon
+ *  that has position and may be visible.
+ * This is to provide a common superclass
+ * to various classes that would otherwise
+ * cause duplicate code, such as items, staircases,
+ * traps, etc
+ */
 class Feature {
 	public:
+		/** Constructor for symbol, location. */
 		Feature(char, Coord);
+		/** Getter for symbol.
+		 * @see symbol
+		 */
 		char getSymbol();
+		/** Getter for location.
+		 * @see location
+		 */
 		Coord getLocation();
+		/** Setter for location.
+		 * @see location
+		 */
 		void setLocation(Coord);
+		/** Destructor. */
 		virtual ~Feature();
 	private:
+		/** Position of the feature within the level. */
 		Coord location;
+		/** Character representing 
+		 *  the feature in the display.
+		 */
 		char symbol;
 };
