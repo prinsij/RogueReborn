90b4b5e897c26df299b0836ebfb5044fcf9a0fcc
prinsij <prinsij@mcmaster.ca>
2016-11-09 11:58:32 -0600

    comment invscreen.h

--- src/include/invscreen.h
+++ src/include/invscreen.h
@@ -13,12 +13,29 @@
 #include "playstate.h"
 #include "uistate.h"
 
+/** Interface state for viewing the contents of
+ *  the player inventory.
+ */
+
 class InvScreen : public UIState {
 	public:
+		/** Constructor.
+		 * We take the playerchar and level so
+		 * we can restore them once gameplay
+		 * resumes.
+		 */
 		InvScreen(PlayerChar*, Level*);
+		/** Draw the inventory.
+		 * Shows like-and-stackable items grouped.
+		 * Makes sure to not reveal the true names
+		 * of undiscovered items.
+		 */
 		void draw(TCODConsole*);
+		/** Handle input (just the quit key). */
 		UIState* handleInput(TCOD_key_t);
 	private:
+		/** Reference to player for when gameplay resumes. */
 		PlayerChar* player;
+		/** Reference to level for when gameplay resumes. */
 		Level* level;
 };
