71ee0ba465650ddd0deb2cdf915d962140907d5f
Mikhail Andrenkov <Mikhail Andrenkov>
2016-11-09 22:18:06 -0500

    Commented scroll.h

--- src/include/ring.h
+++ src/include/ring.h
@@ -13,18 +13,49 @@
 #include "coord.h"
 #include "item.h"
 
-// <Name>
+/** Tuple representing Ring information */
 using RING_TUPLE_TYPE = std::tuple<std::string>;
 
 class Level;
 
+/**
+ * Represents a Ring item.
+ */
 class Ring : public Item {
 	public:
+		/**
+		 * @brief      Constructs a Ring instance with a random type.
+		 *
+		 * @param[in]  location  Ring location
+		 */
 		Ring(Coord);
+
+		/**
+		 * @brief      Constructs a Ring instance.
+		 *
+		 * @param[in]  location  Ring location
+		 * @param[in]  context  Ring context
+		 * @param[in]  type  Ring type
+		 */
 		Ring(Coord, Item::Context, int);
+
+		/**
+		 * @brief      Applies the effects derived from equipping this Ring. 
+		 *
+		 * @param      level  Reference to the Level instance
+		 *
+		 * @return     'true' if the Ring effect was successfully applied; otherwise, 'false'.
+		 */
 		bool activate(Level*);
 
 	private:
+		/**
+		 * Vector of tuples representing different Ring configurations indexed by type.
+		 */
 		static std::vector<RING_TUPLE_TYPE > typeVector;
+
+		/**
+		 * Vector of names to be applied to Rings that are unidentified indexed by type.
+		 */
 		static std::vector<std::string> nameVector;
 };
--- src/include/scroll.h
+++ src/include/scroll.h
@@ -14,21 +14,61 @@
 #include "coord.h"
 #include "item.h"
 
-// <Name>
+/** Tuple representing Scroll information */
 using SCROLL_TUPLE_TYPE = std::tuple<std::string>;
 
 class Level;
 
+/**
+ * Represents a Scroll item.
+ */
 class Scroll : public Item {
 	public:
+		/**
+		 * @brief      Initializes the unidentified names of each Scroll.
+		 *
+		 * @return     Returns a vector of strings denoting random Scroll names indexed by type.
+		 */
 		static std::vector<std::string> initializeScrollNames();
 
+		/**
+		 * @brief      Constructs a Scroll instance with a random type.
+		 *
+		 * @param[in]  location  Scroll location
+		 */
 		Scroll(Coord);
+
+		/**
+		 * @brief      Constructs a Scroll instance.
+		 *
+		 * @param[in]  location  Scroll location
+		 * @param[in]  context  Scroll context
+		 * @param[in]  type  Scroll type
+		 */
 		Scroll(Coord, Item::Context, int);
+
+		/**
+		 * @brief      Applies the effects derived from reading this Scroll. 
+		 *
+		 * @param      level  Reference to the Level instance
+		 *
+		 * @return     A value reflecting the success of the activation operation.
+		 */
 		bool activate(Level*);
 
 	private:
+		/**
+		 * Vector of tuples representing different Scroll configurations indexed by type.
+		 */
 		static std::vector<SCROLL_TUPLE_TYPE > typeVector;
+		
+		/**
+		 * Vector of names to be applied to Scrolls that are unidentified indexed by type.
+		 */
 		static std::vector<std::string> nameVector;
+
+		/**
+		 * Vector of syllables to be used when constructing the randomized Scroll names.
+		 */
 		static std::vector<std::string> syllableVector;
 };
