700847879c9e9c0d8473a6ca8f53111d4236454b
Or Almog <almogo@mcmaster.ca>
2016-11-11 20:31:14 -0500

    documented room

--- src/include/room.h
+++ src/include/room.h
@@ -18,19 +18,57 @@ class Room {
 		enum Darkness {DARK, LIT};
 		enum Treasure {TREASURE, WORTHLESS};
 		enum Hidden {HIDDEN, VISIBLE};
+
 		Room(Coord, Coord, Darkness, Treasure, Hidden, Coord, bool);
 		Room(Coord, Coord);
 		Coord operator[](int);
+
+		/**
+		 * @brief      Clears a passable room in the designated level
+		 *
+		 * @param      Level The level in which to dig
+		 */
 		void dig(Level&);
+
 		Coord getPosition1();
 		Coord getPosition2();
 		Coord getRoomSize();
 		Coord getRoomIndex();
+
+		/**
+		 * @brief      A non-existent room is one which is a 1x1 tunnel tile.
+		 *
+		 * @return     True if the room is real, false if it is simply a tunnel piece.
+		 */
 		bool exists();
+
+		/**
+		 * @brief      Tells you whether or not the coordinate touches the room
+		 *
+		 * @param[in]  Coord The location to test
+		 *
+		 * @return     True if coord can touch or intersect with the room, false otherwise
+		 */
 		bool touches(Coord);
+
+		/**
+		 * @brief      A diagnostic tool
+		 *
+		 * @param[in]  An integer to go along with the info (Used when printing info of multiple rooms).
+		 */
 		void printInfo(int);
+
+		/**
+		 * @brief      Tells you whether or not the coordinate is contained by the room
+		 *
+		 * @param      Coord The coordinate to test
+		 *
+		 * @return     True if the input is within the room, false otherwise.
+		 */
 		bool contains(Coord&, int border=0);
+
 		Darkness getDark();
+		
 	private:
 		Coord topLeft;
 		Coord bottomRight;
