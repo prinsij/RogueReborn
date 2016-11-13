c44f76bba2351f89fe23be4dc96efa1cf9789c81
Ian Prins <prinsij@mcmaster.ca>
2016-11-13 15:39:30 -0500

    replace prompt with subclassing

--- src/invscreen.cpp
+++ src/invscreen.cpp
@@ -10,10 +10,19 @@
 #include "include/playerchar.h"
 #include "include/playstate.h"
 #include "libtcod/include/libtcod.hpp"
+#include <functional>
 
 InvScreen::InvScreen(PlayerChar* player, Level* level)
 	: player(player)
 	, level(level)
+	, filter([] (Item* item) {return true;})
+{}
+
+
+InvScreen::InvScreen(PlayerChar* player, Level* level, std::function<bool(Item*)> filter)
+	: player(player)
+	, level(level)
+	, filter(filter)
 {}
 
 UIState* InvScreen::handleInput(TCOD_key_t key) {
@@ -26,6 +35,7 @@ UIState* InvScreen::handleInput(TCOD_key_t key) {
 void InvScreen::draw(TCODConsole* con) {
 	int y = 0;
 	for (auto& pair : player->getInventory().getContents()) {
+		if (not filter(pair.second.item)) continue;
 		con->print(0, y,
 			(std::string(1, pair.first)+") "+std::to_string(pair.second.quantity)+" "
 			+pair.second.item->getDisplayName()).c_str());
