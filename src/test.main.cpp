d062dfe417b7ba3e24006c7511e7b4e7de76fc28
Or Almog <almogo@mcmaster.ca>
2016-11-15 18:28:08 -0500

    Moved test files to main directory

---
+++ src/test.main.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file test.main.cpp
+ * @author Team Rogue++
+ * @date November 13, 2016
+ *
+ * @brief Global members
+ */ 
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "test.armor.cpp"
+#include "test.level.cpp"
+#include "test.testable.cpp"
+
+int main(){
+
+	ArmorTest a = ArmorTest();
+	a.test();
+
+
+	LevelTest l = LevelTest();
+	l.test();
+
+	return 0;
+}
