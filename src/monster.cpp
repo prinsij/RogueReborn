2431129104fe2d0e369f084ddc38a1f8cb4d6c07
Ian Prins <prinsij@mcmaster.ca>
2016-11-18 10:01:54 -0500

    fix bug with monster theft

--- src/monster.cpp
+++ src/monster.cpp
@@ -233,7 +233,7 @@ void Monster::attackSteal(Level* level) {
 	if (Generator::intFromRange(0, 99) <= 50) {
 		if (Generator::intFromRange(0, 99) <= 10) return;
 
-		ItemZone inventory = player->getInventory();
+		ItemZone& inventory = player->getInventory();
 
 		std::vector<Item*> playerItems;
 
@@ -251,11 +251,9 @@ void Monster::attackSteal(Level* level) {
 		stolenItem->setContext(Item::FLOOR);
 		stolenItem->setLocation(getLocation());
 
-		// player->appendLog("Your supplies feel lighter");
+		player->appendLog("Your supplies feel lighter");
 
-		// std::cout << "Monster attempted to steal " << stolenItem->getName() << "\n";
-	
-		//delete stolenItem;
+		std::cout << "Monster attempted to steal " << stolenItem->getName() << "\n";
 	} else {
 		if (player->getGold() <= 0 || Generator::intFromRange(0, 99) <= 10) return;
 
